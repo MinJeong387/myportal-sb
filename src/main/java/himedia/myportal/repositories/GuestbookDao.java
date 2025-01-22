b6cf59f35e9ee96462cc072613006dbaf323ee6d
서민정 <ybsm0629@gmail.com>
2025-01-22 12:59:10 +0900

    first commit

---
+++ src/main/java/himedia/myportal/repositories/GuestbookDao.java
@@ -0,0 +1,14 @@
+package himedia.myportal.repositories;
+
+import java.util.List;
+
+import himedia.myportal.repositories.vo.GuestbookVo;
+
+public interface GuestbookDao {
+	public List<GuestbookVo> selectAll();
+
+	public int insert(GuestbookVo vo);
+
+	public int delete(GuestbookVo vo);
+
+}
