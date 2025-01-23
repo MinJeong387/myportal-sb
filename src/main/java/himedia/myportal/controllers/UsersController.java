5f5ad576f88476706007c795e45e7cd81cd0dc89
서민정 <ybsm0629@gmail.com>
2025-01-23 16:12:05 +0900

    MVC Config

--- src/main/java/himedia/myportal/controllers/UsersController.java
+++ src/main/java/himedia/myportal/controllers/UsersController.java
@@ -1,12 +1,31 @@
 package himedia.myportal.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import himedia.myportal.repositories.vo.UserVo;
+import himedia.myportal.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
 public class UsersController {
-	/*
 	private static final Logger logger =
 			LoggerFactory.getLogger(UsersController.class);
 	@Autowired
@@ -101,19 +120,4 @@ public class UsersController {
 		
 		return map;
 	}
-	*/
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
